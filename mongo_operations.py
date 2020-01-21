82a31b36d87d3d23a868adc3393a375130514204
Navneet Gupta <navneet.gupta@reedbusiness.com>
2020-01-21 07:06:13 +0530

    added support for speaker

--- mongo_operations.py
+++ mongo_operations.py
@@ -8,15 +8,17 @@ client = MongoClient(f'mongodb+srv://user1:{password}@cluster0-bf7gi.mongodb.net
 db = client['InteractivityChallenge']
 collection = db['individualUser']
 
-def insert(transcript, sentiment_polarity, emoji):
+def insert(transcript, sentiment_polarity, emoji, timestamp):
     _id=uuid.uuid4()
     record = {
         '_id': str(_id),
         'transcript': transcript,
         'sentiment_polarity': sentiment_polarity,
-        'emoji': emoji
+        'emoji': emoji,
+        'timestamp': timestamp
     }
     collection.insert_one(record)
+    print('--------------')
 
 def find():
     record = collection.find({})
@@ -24,7 +26,7 @@ def find():
         print(doc)
     return record
 
-def augment_and_insert(transcript):
+def augment_and_insert(transcript, timestamp):
     sentiment_polarity = calculate_sentiment_polarity(transcript)
     emoji = emoji_mapper(sentiment_polarity)
-    insert(transcript, sentiment_polarity, emoji)
+    insert(transcript, sentiment_polarity, emoji, timestamp)
